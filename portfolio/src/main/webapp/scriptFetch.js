18ba87fa48e6626e7ee24a12f497e14316de2328
sym1000 <santiagoyeomansmolina@gmail.com>
2021-03-07 21:49:48 +0000

    Do JSON walkthrough

--- portfolio/src/main/webapp/scriptFetch.js
+++ portfolio/src/main/webapp/scriptFetch.js
@@ -1,8 +1,18 @@
 /* Gets a string to display on page  */
-async function fetch() {
+async function getRandomString() {
 
-  const textFromResponse = "This is a random string";
+  //const textFromResponse = "This is a random string";
+
+  // Send a request to /random-string.
+  const responseFromServer = await fetch('/random-string');
+    
+  // Parse the response as JSON.
+  const messages = await responseFromServer.json();
+
+    const randomString = words[Math.floor(Math.random() * messages.length)];
+    console.log(randomString);
+    alert(randomString);
 
   const dateContainer = document.getElementById('string-container');
-  dateContainer.innerText = textFromResponse;
+  dateContainer.innerText = randomString;
 }
