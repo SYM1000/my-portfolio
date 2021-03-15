fa96111faf2d1c10714db18fd2d39baeff44ff3c
sym1000 <santiagoyeomansmolina@gmail.com>
2021-03-15 21:24:02 +0000

    Resolve: Remove commented out code

--- portfolio/src/main/webapp/scriptFetch.js
+++ portfolio/src/main/webapp/scriptFetch.js
@@ -1,17 +1,14 @@
 /* Gets a string to display on page  */
 async function getRandomString() {
-
-  //const textFromResponse = "This is a random string";
-
   // Send a request to /random-string.
   const responseFromServer = await fetch('/random-string');
     
   // Parse the response as JSON.
   const messages = await responseFromServer.json();
 
-    const randomString = words[Math.floor(Math.random() * messages.length)];
-    console.log(randomString);
-    alert(randomString);
+  const randomString = words[Math.floor(Math.random() * messages.length)];
+  console.log(randomString);
+  alert(randomString);
 
   const dateContainer = document.getElementById('string-container');
   dateContainer.innerText = randomString;
